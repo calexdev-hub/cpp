ca42030ec3c5b9f3b556259ddfae91286076a401
calexdev-hub <calexdev.contact@gmail.com>
2026-03-12 13:42:57 +0000

    Const

---
+++ const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    const double PI = 3.14;
+    double radius = 2.5;
+    double circuference = PI * (2 * radius);
+
+    std::cout << circuference << "cm";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 640;
+    const int HIGHT = 480;
+
+    return 0;
+}
